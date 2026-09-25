7929c83d61b1cc80acd61ca96973c95990b51ff6
Bryan Tejeda <bryanjose1306@gmail.com>
2026-09-25 10:24:04 -0400

    in this code the "hello wolrd" gonna reproduce 5 times

---
+++ helloWorldFtimes.cpp
@@ -0,0 +1,18 @@
+/*
+Lab #1: hello world 5 times
+author: Bryan Tejeda
+version: 0.1 9/25/2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Hello World!" << endl;
+    }
+
+    return 0;
+}
